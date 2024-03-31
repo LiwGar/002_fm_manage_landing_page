97a41c3cce3d8a96d7dda4d00f5734657f13a348
liwgar <wilenri341@hotmail.com>
2024-03-31 17:03:35 -0500

    Configuracion inicial del proyecto

--- script.js
+++ script.js
@@ -1,44 +1,44 @@
-//toggle icon navbar
+// //toggle icon navbar
 
-let menuIcon = document.querySelector('#menu_Icon');
+// let menuIcon = document.querySelector('#menu_Icon');
 
-let navBar = document.querySelector('.nav_Bar');
+// let navBar = document.querySelector('.nav_Bar');
 
-menuIcon.onclick = () => {
-    menuIcon.classList.toggle('bx-x');
-    //se agrega en el css como nabvar.active
-    navBar.classList.toggle('active');
-}
+// menuIcon.onclick = () => {
+//     menuIcon.classList.toggle('bx-x');
+//     //se agrega en el css como nabvar.active
+//     navBar.classList.toggle('active');
+// }
 
-//scroll Sections
+// //scroll Sections
 
-let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav a');
-
-
-window.onscroll = () => {
-    sections.forEach(section => {
-        let top = window.scrollY;
-        let offset = section.offsetTop - 100;
-        let height = section.offsetHeight;
-        let id = section.getAttribute('id');
-
-        if(top >= offset && top < offset + height) {
-            //active navbar links
-            navLinks.forEach(links => {
-                links.classList.remove('active');
-                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
-            });
-        }
-    });
+// let sections = document.querySelectorAll('section');
+// let navLinks = document.querySelectorAll('header nav a');
 
 
-    //sticky header //se agrega en el css como header.sticky
-    let header = document.querySelector('header');
+// window.onscroll = () => {
+//     sections.forEach(section => {
+//         let top = window.scrollY;
+//         let offset = section.offsetTop - 100;
+//         let height = section.offsetHeight;
+//         let id = section.getAttribute('id');
+
+//         if(top >= offset && top < offset + height) {
+//             //active navbar links
+//             navLinks.forEach(links => {
+//                 links.classList.remove('active');
+//                 document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+//             });
+//         }
+//     });
+
+
+//     //sticky header //se agrega en el css como header.sticky
+//     let header = document.querySelector('header');
     
-    header.classList.toggle('sticky', window.scrollY > 100);
+//     header.classList.toggle('sticky', window.scrollY > 100);
 
-    //remove toggle icon and navbar when click navbar links (scroll)
-    menuIcon.classList.remove('bx-x');
-    navBar.classList.remove('active');
-}
+//     //remove toggle icon and navbar when click navbar links (scroll)
+//     menuIcon.classList.remove('bx-x');
+//     navBar.classList.remove('active');
+// }
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{html,js}',
+    './src/**/*.{html,js,ts,jsx,tsx,mdx}',
+    './**/*.{html,js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
