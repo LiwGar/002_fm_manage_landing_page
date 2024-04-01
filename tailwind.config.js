05ffee1c6a65876e902b7e9f5880c1af5dbf5b1e
liwgar <wilenri341@hotmail.com>
2024-04-01 15:47:34 -0500

    Estilos y responsive design para nav_Bar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{html,js}',
     './src/**/*.{html,js,ts,jsx,tsx,mdx}',
+    './**/*.{html,js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
